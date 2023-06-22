43c9ccb1ca5dd12da06fc7a8e561c7557d541391
rahulbatra-19 <batrarahul409@example.com>
2023-06-22 16:42:09 +0530

    set up the view engine

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 8000;
 
 // use express routes
 app.use('/', require('./routes'));    // by default it fecthes routes/index.js
+
+// set up our view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
 app.listen(port, function(err, data)
 {
     if(err){
