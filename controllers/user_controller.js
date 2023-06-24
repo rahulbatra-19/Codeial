38734548656447d58ac4474215a26a135d3949f9
rahulbatra-19 <batrarahul409@example.com>
2023-06-24 21:01:49 +0530

    Sign Up authentication done

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     // res.end('<h1> User Profile </h1>');
     return res.render('user_profile',
@@ -25,6 +27,28 @@ module.exports.signIn  = function(req, res){
 // get up Sign up data 
 module.exports.create = function(req, res)
 {
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}).then(user=>{
+        if(!user)
+        {
+            User.create(req.body)
+            .then(()=>{ return res.redirect('/users/sign-in');})
+            .catch((err) =>{ 
+                console.log('Error in creating user while signing up');
+            return;
+        });
+        }
+        else{
+            return res.redirect('back');
+        }
+    }).catch(err =>{
+        console.log('Error in finding user in signing up');
+        return;
+    });
+        
+
 
 }
 
