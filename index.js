b9eb6287545fb330b94640e46862bbd39599c4ec
rahulbatra-19 <batrarahul409@example.com>
2023-06-24 20:27:18 +0530

    Set up the cookies for the application

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
 
-const expressLayouts = require('express-ejs-layouts');
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 app.use(expressLayouts);
 
 
