a4b96fbd5c7e06c13cea14979efe34ff7b97716c
rahulbatra-19 <batrarahul409@example.com>
2023-07-01 23:45:01 +0530

    Used ajax to create and delete the post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,18 +1,53 @@
 const Post = require('../models/posts');
 const Comment = require('../models/comments');
 
-module.exports.create = function(req, res)
-{
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }).then(()=>{
-        req.flash('success', "Post Created");
-        return res.redirect('back') ;
-    }).catch(err => {
+// module.exports.create = async function(req, res)
+// {
+//     Post.create({
+//         content: req.body.content,
+//         user: req.user._id
+//     }).then(post=>{
+//         if(req.xhr)
+//         {
+//             return res.status(200).json({
+//                 data : {
+//                     post : post
+//                 }, 
+//                 message : "Post Created!"
+//             });
+//         }
+//         req.flash('success', "Post Created");
+//         return res.redirect('back') ;
+//     }).catch(err => {
+//         req.flash('error', err);
+//         return;
+//     });
+// }
+
+module.exports.create = async function(req, res){
+    try{
+        let post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+        
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
+        req.flash('success', 'Post published!');
+        return res.redirect('back');
+
+    }catch(err){
         req.flash('error', err);
-        return;
-    });
+        return res.redirect('back');
+    }
+  
 }
 
 
@@ -31,15 +66,26 @@ module.exports.destroy =  async function(req, res)
             let comment = await Comment.deleteMany({post: req.params.id});
             post.deleteOne();
 
-            req.flash('success', 'Posts and associated comments destroyed');
+            if(req.xhr)
+            {
+                return res.status(200).json({
+                    data : {
+                        post_id : req.params.id
+                    },
+                    message: "Post deleted "
+                });
+            
+            }
+        req.flash('success', 'Posts and associated comments destroyed');
             return res.redirect('back');
+
         }
         else{
             req.flash('error', 'You cannot delete this post');
             return res.redirect('back');
         }
     } catch (error) {
-        req.flash('error', err);
+        req.flash('error', error);
         return res.redirect('back');
     }
 
